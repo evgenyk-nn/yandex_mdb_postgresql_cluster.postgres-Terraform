ed11f18bf4398419937f413abb93d4c8674a9b22
Evgeny Konnov <evgen.nn.ru@rambler.ru>
2023-12-29 22:16:05 +0300

    Added a block to the specification file describing the creation of a PostgreSQL database cluster

--- my-config.tf
+++ my-config.tf
@@ -53,6 +53,52 @@ resource "yandex_vpc_subnet" "subnet-1" {
   network_id     = "${yandex_vpc_network.network-1.id}"
   v4_cidr_blocks = ["10.2.0.0/16"]
 }
+
+resource "yandex_mdb_postgresql_cluster" "postgres-1" {
+  name        = "postgres-1"
+  environment = "PRESTABLE"
+  network_id  = yandex_vpc_network.network-1.id
+ 
+  config {
+    version = 12
+    resources {
+      resource_preset_id = "s2.micro"
+      disk_type_id       = "network-ssd"
+      disk_size          = 16
+    }
+    postgresql_config = {
+      max_connections                   = 395
+      enable_parallel_hash              = true
+      vacuum_cleanup_index_scale_factor = 0.2
+      autovacuum_vacuum_scale_factor    = 0.34
+      default_transaction_isolation     = "TRANSACTION_ISOLATION_READ_COMMITTED"
+      shared_preload_libraries          = "SHARED_PRELOAD_LIBRARIES_AUTO_EXPLAIN,SHARED_PRELOAD_LIBRARIES_PG_HINT_PLAN"
+    }
+  }
+ 
+  database {
+    name  = "postgres-1"
+    owner = "my-name"
+  }
+ 
+  user {
+    name       = "my-name"
+    password   = "Test1234"
+    conn_limit = 50
+    permission {
+      database_name = "postgres-1"
+    }
+    settings = {
+      default_transaction_isolation = "read committed"
+      log_min_duration_statement    = 5000
+    }
+  }
+ 
+  host {
+    zone      = "ru-central1-a"
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+  }
+}
  
 output "internal_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
@@ -61,4 +107,3 @@ output "internal_ip_address_vm_1" {
 output "external_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
- 
